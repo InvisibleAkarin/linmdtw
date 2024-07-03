290aa5678690acd24be5ae9bc73c54dd83949041
wzy <13051498430@163.com>
2024-07-03 23:11:06 +0800

    修改.pyx

--- linmdtw/linmdtwPy2Cpp.pyx
+++ linmdtw/linmdtwPy2Cpp.pyx
@@ -1,10 +1,12 @@
 # distutils: language = c++
 
 cimport linmdtwPy2Cpp
+cimport cnp
 import cython
+import numpy
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def DTW(numpy.ndarray[float,ndim=2,mode="c"] X not None, numpy.ndarray[float,ndim=2,mode="c"] Y not None, bool do_gpu, dict metadata):
+def linmdtw(numpy.ndarray[float,ndim=2,mode="c"] X not None, numpy.ndarray[float,ndim=2,mode="c"] Y not None, bint do_gpu, dict metadata):
     cost, path = linmdtw(X, Y, None, 500, do_gpu, metadata)
     return (cost, path)
