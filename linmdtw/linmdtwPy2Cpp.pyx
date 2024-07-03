b06e8a0b83bbe55f677c4b29c83d276cfb0c2b02
wzy <13051498430@163.com>
2024-07-03 23:16:59 +0800

    刚忘保存了

--- linmdtw/linmdtwPy2Cpp.pyx
+++ linmdtw/linmdtwPy2Cpp.pyx
@@ -7,6 +7,6 @@ import numpy
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def linmdtw(numpy.ndarray[float,ndim=2,mode="c"] X not None, numpy.ndarray[float,ndim=2,mode="c"] Y not None, bint do_gpu, dict metadata):
+def c_linmdtw(numpy.ndarray[float,ndim=2,mode="c"] X not None, numpy.ndarray[float,ndim=2,mode="c"] Y not None, bint do_gpu, dict metadata):
     cost, path = linmdtw(X, Y, None, 500, do_gpu, metadata)
     return (cost, path)
